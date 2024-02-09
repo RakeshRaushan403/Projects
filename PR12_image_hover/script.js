1457c9e6453e99667e8928c2f939ac574fced179
Rakesh Raushan <106985676+Rakesh9608@users.noreply.github.com>
2024-02-09 14:45:56 +0530

    Add files via upload

---
+++ PR12_image_hover/script.js
@@ -0,0 +1,4 @@
+var audio = document.createElement('audio');
+audio.setAttribute('src', 'bg.mp3'); 
+audio.loop = true;
+audio.play(); // Uncomment this line to automatically play the audio when the website is refreshed
